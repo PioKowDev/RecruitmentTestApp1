cdb8dbda60db1d48996812be44ac0b72f79388aa
piotrkowalik <piotr.kowalik@bshg.com>
2017-09-15 18:55:41 +0200

    add support for native view bindings

--- src/app/src/main/java/com/kovalik/rtestapp1/WebPageActivity.java
+++ src/app/src/main/java/com/kovalik/rtestapp1/WebPageActivity.java
@@ -1,13 +1,20 @@
 package com.kovalik.rtestapp1;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.kovalik.rtestapp1.databinding.ActivityWebPageBinding;
 
 public class WebPageActivity extends AppCompatActivity {
+
+    private ActivityWebPageBinding mViewsBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_web_page);
+        mViewsBinding = DataBindingUtil.setContentView(this, R.layout.activity_web_page);
+
+
+
     }
 }
