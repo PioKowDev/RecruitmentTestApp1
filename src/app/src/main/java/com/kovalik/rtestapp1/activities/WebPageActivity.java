179f2deee053d2a3f56075fe4db91f2b44e311ab
piotrkowalik <piotr.kowalik@bshg.com>
2017-09-15 21:57:49 +0200

    first page loadable version

--- src/app/src/main/java/com/kovalik/rtestapp1/activities/WebPageActivity.java
+++ src/app/src/main/java/com/kovalik/rtestapp1/activities/WebPageActivity.java
@@ -4,9 +4,11 @@ import android.databinding.DataBindingUtil;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.jakewharton.rxbinding2.view.RxView;
 import com.kovalik.rtestapp1.R;
 import com.kovalik.rtestapp1.databinding.ActivityWebPageBinding;
 import com.kovalik.rtestapp1.fragments.WebPageFragment;
+import io.reactivex.Observable;
 
 public class WebPageActivity extends AppCompatActivity {
 
@@ -14,6 +16,8 @@ public class WebPageActivity extends AppCompatActivity {
 
     private ActivityWebPageBinding mViewsBinding;
     private WebPageFragment mWebPageFragment;
+
+    private Observable<String> mButtonObservable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +32,13 @@ public class WebPageActivity extends AppCompatActivity {
 
             fm.beginTransaction().add(R.id.pageContentLayout, mWebPageFragment, WEB_PAGE_FRAGMENT_TAG).commit();
         }
+
+        mButtonObservable = RxView.clicks(mViewsBinding.loadPageButton).
+            flatMap(click -> Observable.just(mViewsBinding.pageAddressEditText.getText().toString()));
     }
+
+    public Observable<String> getLoadPageButtonObservable() {
+        return mButtonObservable;
+    }
+
 }
--- src/app/src/main/java/com/kovalik/rtestapp1/fragments/WebPageFragment.java
+++ src/app/src/main/java/com/kovalik/rtestapp1/fragments/WebPageFragment.java
@@ -9,19 +9,41 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebViewClient;
 import com.kovalik.rtestapp1.R;
+import com.kovalik.rtestapp1.activities.WebPageActivity;
 import com.kovalik.rtestapp1.databinding.WebPageFragmentBinding;
-
+import io.reactivex.android.schedulers.AndroidSchedulers;
 
 public class WebPageFragment extends Fragment {
 
-    private WebPageFragmentBinding dataBinding;
+    private WebPageFragmentBinding mViewsBinding;
 
     @Nullable
     @Override
     public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container,
         @Nullable final Bundle savedInstanceState) {
-        dataBinding = DataBindingUtil.inflate(inflater, R.layout.web_page_fragment, null, false);
-        dataBinding.webPage.setWebViewClient(new WebViewClient());
-        return dataBinding.getRoot();
+        mViewsBinding = DataBindingUtil.inflate(inflater, R.layout.web_page_fragment, null, false);
+        mViewsBinding.webPage.setWebViewClient(new WebViewClient());
+        return mViewsBinding.getRoot();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        WebPageActivity parentActivity = (WebPageActivity)getActivity();
+        if (parentActivity != null) {
+            parentActivity.getLoadPageButtonObservable().subscribe(this::loadPage);
+        }
+    }
+
+    public void loadPage(String providedUrl) {
+        mViewsBinding.webPage.loadUrl(providedUrl);
+    }
+
+    @Override
+    public void onPause() {
+        WebPageActivity parentActivity = (WebPageActivity)getActivity();
+        parentActivity.getLoadPageButtonObservable().unsubscribeOn(AndroidSchedulers.mainThread());
+
+        super.onPause();
     }
 }
